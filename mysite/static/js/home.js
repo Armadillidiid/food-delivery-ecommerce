8d2c11b766dc862b7ca55779ef012365a18597a7
Emmanuel Isenah <emmanuelisenah@gmail.com>
2022-11-30 22:44:15 +0100

    Added search to filter out vendors by name

--- mysite/static/js/home.js
+++ mysite/static/js/home.js
@@ -1,8 +1,17 @@
-// Auto submit radio on click
-// const categoryForm = document.querySelector('#searchBoxForm')
-// const categoryRadios = document.querySelectorAll('.form-check-input')
-// for (let radio of categoryRadios) {
-//     radio.addEventListener('change', ()=> {
-//         categoryForm.submit()
-//     })
-// }
+const vendors = document.querySelectorAll('.vendors')
+const searchBar = document.querySelector('#searchBar input')
+
+searchBar.addEventListener('input', function() {
+    let query = this.value
+    query = query.toLowerCase()
+
+    for(let vendor of vendors) {
+        vendor_name = vendor.dataset.vendor.toLowerCase()
+        if (vendor_name.includes(query)) {
+            vendor.classList.remove('visually-hidden')
+        }
+        else {
+            vendor.classList.add('visually-hidden')
+        }
+    }
+})
