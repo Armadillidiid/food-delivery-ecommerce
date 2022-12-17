1b53397eac4a2c766c6f50c5a26aed9a1567a1c0
Emmanuel Isenah <emmanuelisenah@gmail.com>
2022-12-17 21:59:12 +0100

    Fixed node module import bug for vendor management

--- mysite/static/js/profile-vendor.js
+++ mysite/static/js/profile-vendor.js
@@ -1,4 +1,3 @@
-import Cookies from "/static/node_modules/js-cookie/dist/js.cookie.mjs";
 const csrftoken = Cookies.get("csrftoken");
 const openHourOpenTime = document.querySelectorAll(".openHourOpenTime");
 const openHourCloseTime = document.querySelectorAll(".openHourCloseTime");
